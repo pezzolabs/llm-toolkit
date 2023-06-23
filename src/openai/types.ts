135f7d14f270725729bad84a17aea47c02cff284
Itay Elgazar <Itayekk1@gmail.com>
2023-06-23 10:58:49 +0300

    feat: separate models to gpt (#1)

--- src/openai/types.ts
+++ src/openai/types.ts
@@ -1,4 +1,10 @@
-export interface OpenAIModelSettings {
+export interface OpenAIGptModelSettings {
   promptCostPer1000Tokens: number;
   completionsCostPer1000Tokens: number;
+  maxTokens: number;
+}
+
+export interface OpenAIGptCostOptions {
+  promptTokens: number;
+  completionTokens: number;
 }
