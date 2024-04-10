4c9d7c096877a2a0b4ebf25384b16e00aa20cd74
Ariel Weinberger <weinberger.ariel@gmail.com>
2024-04-10 23:10:49 +0200

    update models

--- src/openai/index.ts
+++ src/openai/index.ts
@@ -61,13 +61,28 @@ const openAIToolkit = () => {
     ["gpt-4-1106-preview"]: defineModel<OpenAIGptModelSettings>({
       promptCostPer1000Tokens: 0.01,
       completionsCostPer1000Tokens: 0.03,
-      maxTokens: 4096,
+      maxTokens: 128000,
     }),
     // Same as above
     ["gpt-4-vision-preview"]: defineModel<OpenAIGptModelSettings>({
       promptCostPer1000Tokens: 0.01,
       completionsCostPer1000Tokens: 0.03,
-      maxTokens: 4096,
+      maxTokens: 128000,
+    }),
+    ["gpt-4-turbo-preview"]: defineModel<OpenAIGptModelSettings>({
+      promptCostPer1000Tokens: 0.01,
+      completionsCostPer1000Tokens: 0.03,
+      maxTokens: 128000,
+    }),
+    ["gpt-4-1106-vision-preview"]: defineModel<OpenAIGptModelSettings>({
+      promptCostPer1000Tokens: 0.01,
+      completionsCostPer1000Tokens: 0.03,
+      maxTokens: 128000,
+    }),
+    ["gpt-4-0125-preview	"]: defineModel<OpenAIGptModelSettings>({
+      promptCostPer1000Tokens: 0.01,
+      completionsCostPer1000Tokens: 0.03,
+      maxTokens: 128000,
     }),
   };
 
